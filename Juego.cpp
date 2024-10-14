5193173180e3a4759bae007b8b18805c0418f00a
kiefer1234 <kiefermorales603@gmail.com>
2024-10-14 02:07:52 -0300

    main 1.6
    agregue lo de pedir nombre al usuario y mostrarlo

--- Juego.cpp
+++ Juego.cpp
@@ -17,25 +17,52 @@ void TiradaDeLosDados(int Dados[],int Cantidad)
     }
 }
 ///funcion que tira los dados bloqueadores (la variable esta pasada por referencia)
-void TirarDadosBloqueadores(int (&DadosBloqueadores)[2]){
+void TirarDadosBloqueadores(int (&DadosBloqueadoresJugador1)[2],int (&DadosBloqueadoresJugador2)[2]){
   for(int i=0;i<2;i++){
-    DadosBloqueadores[i]= TirarDado();
+    DadosBloqueadoresJugador1[i]= TirarDado();
+    DadosBloqueadoresJugador2[i]= TirarDado();
   }
 }
-void MostrarDadosBloqueadores(int(&DadosBloqueadores)[2]){
+///funcion que muestra los dados bloqueadores
+void MostrarDadosBloqueadores(int DadosBloqueador[]){
      cout << endl;
 cout << "                      _______" << endl;
 cout << "                     |"<<"   |"<< "   |" <<endl;
-cout << "  Dados bloqueadores:| " << DadosBloqueadores[0] << " | " << DadosBloqueadores[1] << " |" << endl;
+cout << "  Dados bloqueadores:| " << DadosBloqueador[0] << " | " << DadosBloqueador[1] << " |" << endl;
 cout << "                     |___|___|" << endl;
     }
+///pide nombre del jugador
+string PedirNombreJugador(){
+        string nombre;
+      cout<< "ingrese su nombre: ";
+      cin>>nombre;
+
+      return nombre;
+    }
+///muestra el nombre del jugador
+    void MostrarNombreJugador1(string nombre){
+    cout<< "Turno del jugador: "<<nombre<<endl;
+    }
+
 
 ///funcion jugar, es la que se le pasa al menu para que pueda correr el juego
 void JugarContraCPU(){
-    int DadosBloqueadores[2]{};
+///declaracion de variables
+    int DadosBloqueadoresJugador1[2]{};
+    int DadosBloqueadoresJugador2[2]{};
     int cantidadDeDados=5;
     int daditos[cantidadDeDados];
-    TirarDadosBloqueadores(DadosBloqueadores);
-    MostrarDadosBloqueadores(DadosBloqueadores);
+    string nombreJugador;
+///pide el nombre del jugador
+    nombreJugador =  PedirNombreJugador();
+///borra pantalla
+    system("cls");
+///muestra el nombre del jugador
+    MostrarNombreJugador1(nombreJugador);
+///Tira los dados bloqueadores de los dos jugadores y lo guarda en la variables
+    TirarDadosBloqueadores(DadosBloqueadoresJugador1,DadosBloqueadoresJugador2);
+///muestra los dados bloqueadores
+    MostrarDadosBloqueadores(DadosBloqueadoresJugador1);
+///simula una tirada de dados
     TiradaDeLosDados(daditos,cantidadDeDados);
 }
--- Juego.h
+++ Juego.h
@@ -2,5 +2,7 @@
  void JugarContraCPU();
  int TirarDado();
 void TiradaDeLosDados(int Dados[],int Cantidad);
-void TirarDadosBloqueadores(int(&DadosBloqueadores)[2]);
-void MostrarDadosBloqueadores(int(&DadosBloqueadores)[2]);
+void TirarDadosBloqueadores(int (&DadosBloqueadoresJugador1)[2],int (&DadosBloqueadoresJugador2)[2]);
+void MostrarDadosBloqueadores(int DadosBloqueador[]);
+std::string PedirNombreJugador();
+void MostrarNombreJugador1(std::string nombre);
